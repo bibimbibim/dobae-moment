31cf20f9c7c7c5a5e28a3f248e08be8669e542d1
bibimbibim <79782169+bibimbibim@users.noreply.github.com>
2021-11-15 16:01:42 +0900

    console.log 2 times

--- src/background.js
+++ src/background.js
@@ -1,3 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('[background] chrome.runtime.onInstalled initialize');
+    console.log('[background] chrome.runtime.onInstalled initialize');
 });
