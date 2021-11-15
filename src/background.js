cb6f7d60d7e6d676a15c9d169dad1d907738bab6
bibimbibim <79782169+bibimbibim@users.noreply.github.com>
2021-11-16 02:08:13 +0900

    add text chrome

--- src/background.js
+++ src/background.js
@@ -2,3 +2,5 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log('[background] chrome.runtime.onInstalled initialize');
     console.log('[background] chrome.runtime.onInstalled initialize');
 });
+
+chrome
